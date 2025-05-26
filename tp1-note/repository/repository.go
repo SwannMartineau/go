4f159d0b069976f4aa6af4f5c5a03b9e90d4242f
Swann-Mistertemp <swann.martineau@mistertemp.com>
2025-05-26 16:15:56 +0200

    init: functions

--- tp1-note/repository/repository.go
+++ tp1-note/repository/repository.go
@@ -1,1 +1,31 @@
 package repository
+
+type Person struct {
+	Name        string
+	FirstName   string
+	PhoneNumber string
+}
+
+type Repository struct {
+	persons []Person
+}
+
+func listRepository(persons []Person) []Person {
+	return persons
+}
+
+func addPerson(persons []Person, person Person) []Person {
+	return persons
+}
+
+func deletePerson(persons []Person, person Person) []Person {
+	return persons
+}
+
+func modifyPerson(persons []Person, person Person) []Person {
+	return persons
+}
+
+func searchPerson(persons []Person, person Person) []Person {
+	return persons
+}
