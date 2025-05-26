001b6f4a826c67f996ddd2f311190ae8bf38f5ad
Swann-Mistertemp <swann.martineau@mistertemp.com>
2025-05-26 17:25:53 +0200

    feat: add flags

--- tp1-note/main.go
+++ tp1-note/main.go
@@ -1,47 +1,65 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/SwannMartineau/go/tp1-note/repository"
 )
 
 func main() {
-	person1 := repository.Person{
-		Name:        "Dupont",
-		FirstName:   "Jean",
-		PhoneNumber: "0123456789",
+	actionPtr := flag.String("action", "", "Action à effectuer (ajouter, rechercher, lister)")
+	nomPtr := flag.String("nom", "", "Nom de famille de la personne")
+	prenomPtr := flag.String("prenom", "", "Prénom de la personne")
+	telPtr := flag.String("tel", "", "Numéro de téléphone")
+
+	flag.Parse()
+
+	persons := loadDirectory()
+
+	newPerson := repository.Person{
+		Name:        *nomPtr,
+		FirstName:   *prenomPtr,
+		PhoneNumber: *telPtr,
 	}
 
-	person2 := repository.Person{
-		Name:        "Martin",
-		FirstName:   "Sophie",
-		PhoneNumber: "0987654321",
+	switch *actionPtr {
+	case "ajouter":
+		if *nomPtr == "" {
+			fmt.Println("Erreur: Le nom est requis pour ajouter une personne")
+		}
+
+		persons = repository.AddPerson(persons, newPerson)
+		fmt.Printf("Personne ajoutée: %s %s, Tél: %s\n", *prenomPtr, *nomPtr, *telPtr)
+		repository.ListRepository(persons)
+
+	case "rechercher":
+		if *nomPtr == "" && *prenomPtr == "" {
+			fmt.Println("Erreur: Le nom ou le prénom est requis pour rechercher une personne")
+		}
+
+		repository.SearchPerson(persons, newPerson)
+
+	case "lister":
+		repository.ListRepository(persons)
+
+	case "supprimer":
+		persons = repository.DeletePerson(persons, newPerson)
+		repository.ListRepository(persons)
+
+	case "modifier":
+		persons = repository.ModifyPerson(persons, newPerson.Name, newPerson.FirstName, *telPtr)
+		repository.ListRepository(persons)
+
+	default:
+		fmt.Println("Action non reconnue. Utilisez 'ajouter', 'rechercher', 'lister', 'supprimer' ou 'modifier'")
 	}
+}
 
-	persons := []repository.Person{person1, person2}
-
-	repository.ListRepository(persons)
-
-	persons2 := []repository.Person{}
-
-	repository.ListRepository(persons2)
-
-	persons2 = repository.AddPerson(persons2, person1)
-
-	repository.ListRepository(persons2)
-
-	persons2 = repository.AddPerson(persons2, person1)
-
-	repository.ListRepository(persons2)
-
-	persons2 = repository.DeletePerson(persons2, person1)
-
-	repository.ListRepository(persons2)
-
-	persons = repository.ModifyPerson(persons, person1.Name, person1.FirstName, "66666666")
-
-	repository.ListRepository(persons)
-
-	repository.SearchPerson(persons, person2)
-
-	repository.SearchPerson(persons, person1)
+func loadDirectory() []repository.Person {
+	return []repository.Person{
+		{Name: "Dupont", FirstName: "Jean", PhoneNumber: "0123456789"},
+		{Name: "Martin", FirstName: "Sophie", PhoneNumber: "0987654321"},
+		{Name: "Durand", FirstName: "Alice", PhoneNumber: "0654321987"},
+	}
 }
